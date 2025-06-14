fb97f3aaee3aeff4c09e3a1b444b46fde863e056
bokkonotty <105218167+antonioTriozzi@users.noreply.github.com>
2025-06-14 14:10:29 +0200

    changes

--- config/config.php
+++ config/config.php
@@ -1,8 +1,9 @@
 <?php
 
-
 //Database configuration
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'progetto_missouri');
+define('DB_NAME', 'demoproject');
 define('DB_USER', 'root');
-define('DB_PASS', 'pippo');
+define('DB_PASS', '');
+
+?>
