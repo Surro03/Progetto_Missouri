663cf8921a61296091c3879a72666230ba037e1f
Surro03 <samuele2003@icloud.com>
2025-06-14 19:31:12 +0200

    StudyRoom

---
+++ Foundation/FStudyRoom.php
@@ -0,0 +1,28 @@
+<?php
+class FStudent {
+    private static $table = "studyrooms";
+
+    private static $value = "(:studyRoomId, :openingTime, :closingTime, :tableList)";
+
+    private static $key = "studyRoomId";
+
+    
+
+    public function __construct() {
+        // Constructor can be used for initialization if needed
+    }
+    
+    public static function getTable() {
+        return self::$table;
+    }
+    public static function getValue() {
+        return self::$value;
+    }
+    public static function getKey() {
+        return self::$key;
+    }
+    public static function getClass(){
+        return self::class;
+    }
+
+}
